cd74062a7ff6b22d7ba9109663cb4551cd7ec956
ChrisKent <chrisgkent@gmail.com>
2025-06-06 12:49:54 +0100

    Added / tested expand ambs

--- primalbedtools/utils.py
+++ primalbedtools/utils.py
@@ -18,6 +18,45 @@ AMBIGUOUS_DNA_COMPLEMENT = {
     "-": "-",
 }
 
+ALL_DNA_WITH_N: dict[str, str] = {
+    "A": "A",
+    "C": "C",
+    "G": "G",
+    "T": "T",
+    "M": "AC",
+    "R": "AG",
+    "W": "AT",
+    "S": "CG",
+    "Y": "CT",
+    "K": "GT",
+    "V": "ACG",
+    "H": "ACT",
+    "D": "AGT",
+    "B": "CGT",
+    "N": "ACGT",
+}
+
+
+def expand_ambiguous_bases(seq: str) -> list[str]:
+    """
+    Expand ambiguous bases in a DNA sequence to all possible combinations.
+    """
+    if not seq:
+        return []
+    # Initialize the list with the first character
+    expanded = [b for b in ALL_DNA_WITH_N[seq[0]]]
+    # Iterate through the sequence and expand ambiguous bases
+    for base in seq[1:]:
+        new_expanded = []
+        for current in expanded:
+            for option in ALL_DNA_WITH_N[base]:  # raise KeyError
+                new_expanded.append(
+                    current + option
+                )  # slow but not performance critical
+
+        expanded = new_expanded
+    return expanded
+
 
 def rc_seq(seq: str) -> str:
     """
